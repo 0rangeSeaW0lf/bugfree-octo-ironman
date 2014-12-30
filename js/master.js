a66f694eae76c0963322ebc3720b882fe49607d6
Jose Moreno <jose.moreno@reyesic.net>
2014-12-30 20:47:13 +0000

    Add and Validate meal's input

--- js/master.js
+++ js/master.js
@@ -1,11 +1,28 @@
 angular.module('myApp',['ngMessages']).controller('FormCtrl', function($scope){
-    $scope.inputs = {meal:'',tax:'',tip:''};
-}).controller('calculateCtrl', function($scope){
+    $scope.master = {};
+    
+    $scope.add = function(meal, isValid) {
+        if (isValid) {
+            $scope.master = angular.copy(meal);
+        }
+    };
+    
+    $scope.reset = function() {
+        $scope.master = {};
+    };
+    
+    $scope.reset();
+		
+}).controller('CalculateCtrl', function($scope){
+    $scope.$on('displayData', function(event) {
+				$scope.content = true;
+	});
     $scope.calculateSubtotal = function(meal,tax) {
         return meal + (1*tax/100);
     };
     $scope.calculateTip = function(subtotal, tip) {
         return subtotal*tip/100;
     };
+    
     $scope.content = false;
 });
