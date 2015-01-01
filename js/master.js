bca26213fba63b2f365a3bc342d9927cc6e020c5
Jose Moreno <jose.moreno@reyesic.net>
2015-01-01 16:53:56 +0000

    Add basic functionality on calculating stuff

--- js/master.js
+++ js/master.js
@@ -1,28 +1,30 @@
-angular.module('myApp',['ngMessages']).controller('FormCtrl', function($scope){
-    $scope.master = {};
+angular.module('myApp',['ngMessages']).controller('FormCtrl', function($scope,$rootScope){
+    // $scope.master = {};
     
     $scope.add = function(meal, isValid) {
         if (isValid) {
-            $scope.master = angular.copy(meal);
+            $rootScope.$broadcast('calculateCost', meal);
         }
     };
     
     $scope.reset = function() {
-        $scope.master = {};
+        $scope.meal = {};
     };
     
     $scope.reset();
 		
 }).controller('CalculateCtrl', function($scope){
-    $scope.$on('displayData', function(event) {
-				$scope.content = true;
+    $scope.$on('calculateCost', function(event, meal) {
+		$scope.master = meal;
+		$scope.content = true;
+		$scope.subtotal = $scope.master.cost * (1 + $scope.master.tax/100);
+		$scope.tip = $scope.subtotal*$scope.master.tip/100;
+		$scope.earnings.mealCount++;
+		$scope.earnings.tipTotal += $scope.tip;
+		$scope.earnings.averageMealTipe = $scope.earnings.tipTotal / $scope.earnings.mealCount;
 	});
-    $scope.calculateSubtotal = function(meal,tax) {
-        return meal + (1*tax/100);
-    };
-    $scope.calculateTip = function(subtotal, tip) {
-        return subtotal*tip/100;
-    };
+	
+	$scope.earnings = {tipTotal: 0, mealCount: 0, averageMealTipe: 0};
     
     $scope.content = false;
 });
