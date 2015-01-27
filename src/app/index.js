6acd9d5d10b23bf7e45753c7abb213218b218f86
José Moreno <jose.moreno@reyesic.net>
2015-01-27 15:57:48 +0000

    Fix rediraction

--- src/app/index.js
+++ src/app/index.js
@@ -19,7 +19,10 @@ angular.module('workspace', ['ngAnimate', 'ngSanitize', 'ngRoute', 'mgcrea.ngStr
         redirectTo: '/'
       });
   })
-  .run(function($rootScope){
+  .run(function($rootScope, $location) {
+      $rootScope.$on('$routeChangeError', function() {
+          $location.path('/');
+      });
       $rootScope.earnings = {tipTotal: 0, mealCount: 0, averageMealTipe: 0};
       $rootScope.subtotal = 0;
       $rootScope.tip = 0;
