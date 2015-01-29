d4206fad42c2b0773c44f813cb01c6205969083c
José Moreno <jose.moreno@reyesic.net>
2015-01-29 18:48:50 +0000

    Minor changes 3.0

--- src/app/index.js
+++ src/app/index.js
@@ -3,26 +3,26 @@
 angular.module('workspace', ['ngAnimate', 'ngSanitize', 'ngRoute', 'mgcrea.ngStrap', 'ngMessages'])
   .config(function ($routeProvider, $locationProvider) {
     $routeProvider
-      .when('#/', {
+      .when('./', {
         templateUrl: 'app/home/home.html',
         controller: 'HomeCtrl'
       })
-      .when('#/new-meal', {
+      .when('./new-meal', {
         templateUrl: 'app/new-meal/new-meal.html',
         controller: 'NewMealCtrl'
       })
-      .when('#/my-earnings', {
+      .when('./my-earnings', {
         templateUrl: 'app/my-earnings/my-earnings.html',
         controller: 'EarningsCtrl'
       })
       .otherwise({
-        redirectTo: '#/'
+        redirectTo: './'
       });
       $locationProvider.html5Mode({ enabled: true, requireBase: false }).hashPrefix('!');
   })
   .run(function($rootScope, $location, $timeout) {
       $rootScope.$on('$routeChangeError', function() {
-          $location.path('#/');
+          $location.path('./');
       });
 
       $rootScope.earnings = {tipTotal: 0, mealCount: 0, averageMealTipe: 0};
